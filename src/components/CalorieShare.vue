5f3da901b147dc5847624c38d0b8d36a9d91116f
ASO1701016 <1701016@st.asojuku.ac.jp>
2020-09-16 09:30:09 +0900

    [ADD]CalorieShareにsns共有のコードを記載

--- src/components/CalorieShare.vue
+++ src/components/CalorieShare.vue
@@ -1,11 +1,28 @@
 <template>
     <div>
-
+        <a :href="twUrl"></a>
     </div>
 </template>
 
 <script>
     export default {
-        name: "CalorieShare"
+        name: "CalorieShare",
+        data : function(){
+        return {
+            // Twitterのシェア用URL 空でもOK
+            twUrl : '',
+        }
+    },
+        methods : {
+
+            createSnsUrl : function(){
+                // 今のurlエンコード
+                var url = encodeURIComponent(location.href);
+                // タイトル又はdescription
+                var txt = encodeURIComponent(document.querySelector("meta[name='description']").getAttribute('content'));
+                // Twitter用url作成
+                this.twUrl = 'https://twitter.com/intent/tweet?text=' + txt + '&hashtags=' + txt + '&url=' + url;
+            }
+        }
     }
 </script>
