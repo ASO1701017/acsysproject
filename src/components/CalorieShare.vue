55cc9112df63e092eb49f202ac7bcc2ce8698821
ASO1701016 <1701016@st.asojuku.ac.jp>
2020-09-16 14:24:05 +0900

    [FIX]textareaの値をtwitterへ移行する機能完成

--- src/components/CalorieShare.vue
+++ src/components/CalorieShare.vue
@@ -1,19 +1,23 @@
 <template>
-    <div class="twitter_share">
-        <textarea id="detail" v-model="TweeteDetail" maxlength="280" placeholder="SNSで共有する内容"></textarea>
+    <div id="twitter_share">
+        <input type="text" id="detail" v-model="TweetDetail" maxlength="280" placeholder="SNSで共有する内容" />
         <button @click="twitterShare">ツイッターでシェアする</button>
         </div>
 </template>
 
 <script>
     export default {
-
-        methods:{
+        name: "Training",
+        data() {
+            return {
+                TweetDetail: "",
+            }
+        },methods:{
             twitterShare(){
                 //シェアするためのtwitterの画面を設定（textareaに記載された内容付きで）
-                var shareURL = 'https://twitter.com/intent/tweet?text=' + "テスト";
+                var shareURL = 'https://twitter.com/intent/tweet?text=' + this.TweetDetail + "%20%23テスト用";
                 //シェア用の画面へ移行
-                window.open(shareURL)
+                window.open(shareURL,null,'left=350,top=200,width=700,height=550')
                 // location.href = shareURL
                 // this.$router.replace("/savecalorie")
             }
