60da4b7e56a50d412af4b7e36b988bdfb6b55598
raige <raigekka0128@yahoo.co.jp>
2021-01-20 14:42:58 +0900

    -delete LoginHeader.vue：ユーザー情報取得を移行。

--- src/components/LoginHeader.vue
+++ src/components/LoginHeader.vue
@@ -5,7 +5,6 @@
                 <!--ファビコン-->
                 <img src="../../public/favicon.png" width="35" height="35" class="d-inline-block align-top" alt="penguin">
                 acsys</a>
-
             <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
             <b-collapse id="nav-collapse" is-nav>
                 <b-navbar-nav>
@@ -15,7 +14,6 @@
                     <b-nav-item href="/training">トレーニング</b-nav-item>
                     <b-nav-item href="/tweet">Twitterに投稿</b-nav-item>
                 </b-navbar-nav>
-
                 <b-navbar-nav class="ml-auto">
                     <b-nav-item-dropdown right>
                         <!--右側のメニュー-->
@@ -59,49 +57,7 @@
         data() {
             return {
                 modal: false,
-                //通信に関数
-                dataGet:[],
-                userToken:this.$store.state.accountToken,
-                userInfBox:[],
             }
-        },
-        async created() {
-            //ユーザー情報取得
-            const URL = "https://fat3lak1i2.execute-api.us-east-1.amazonaws.com/acsys/users/information"
-            this.dataGet={
-                account_token:this.userToken
-            }
-            const json_data = JSON.stringify(this.dataGet)
-            await fetch(URL,{
-                mode:'cors',
-                method:'POST',
-                body:json_data,
-                headers:{'Content-type':'application'},
-            })
-                .then(response => response.json())
-                .then(data => {
-                    // console.log(data)
-                    const flg_data = data['isSuccess']
-                    if(flg_data){
-                        console.log('ユーザー情報取得:ok')
-                        //ストアにユーザー情報を保存する処理
-                        this.userInfBox={
-                            name:data['account_name'],
-                            birthday:data['account_birthday'],
-                            gender:data['account_gender'],
-                            height:data['account_height'],
-                            weight:data['account_weight'],
-                            activlevel:data['account_level'],
-                            startday:data['regist_date']
-                        }
-                        this.$store.commit('accountUpdate',this.userInfBox)
-                    }else {
-                        console.log('ユーザー情報取得:ng')
-                    }
-                })
-                .catch(function (error) {
-                    console.log(error)
-                })
         },
         methods:{
             //モーダルに関する処理
@@ -120,7 +76,7 @@
 
                 const URL = "https://fat3lak1i2.execute-api.us-east-1.amazonaws.com/acsys/users/logout"
                 this.dataGet={
-                    account_token:this.userToken
+                    account_token:this.$store.state.accountToken
                 }
                 const json_data = JSON.stringify(this.dataGet)
                 fetch(URL,{
