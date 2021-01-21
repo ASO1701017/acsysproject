bfc2617b9001273943c899cd03a830287e1a4458
raige <raigekka0128@yahoo.co.jp>
2021-01-21 17:22:08 +0900

    +Update CountTraining.vue:デザイン完成

--- src/components/CountTraining.vue
+++ src/components/CountTraining.vue
@@ -1,24 +1,24 @@
 <template>
     <div class="container">
         <!-- 姿勢判定機能リンク -->
-        <div class="row">
-            <label class="col-lg-3 col-auto col-form-label" >身体活動レベル</label>
+        <div class="text-success pt-5  mb-3 text-center h1 font-weight-bold">
+            回数カウント(デモ版)
+        </div>
+        <div class="text-success pt-5  mb-3 text-center text-danger h5 font-weight-bold">
+            （注意）デモ版と書いてある通り、まだ完成してないです。そのためおかしい動作があります。<br>
+            又カメラを使用するため、動作が重たいです。
+        </div>
+        <div class="row mt-5">
+            <label class="col-lg-3 col-auto col-form-label" >トレーニング名</label>
             <select v-bind:key="form.id" name=”TrainingDetail” v-model="form.training_detail"  class="form-control col-sm-7 text-left">
                 <option value=1 selected="selected" >スクワット</option>
                 <option value=2>腕立て伏せ</option>
                 <option value=3>腹筋</option>
             </select>
+        </div>
 
-            <div class="row mt-3 mb-5">
-                <button id="send_btn" class="btn btn-success" @click="new SendData(form.id)">決定</button>
-            </div>
-
-            <!--            <button id="droplist_button" class="btn btn-success col-8 mx-auto" @click="dropList"></button>-->
-
-            <div class="text-center mt-3" >
-                <!--              <button id="post_btn" class="btn btn-success col-8 mx-auto"  name="SetMenu" @click="NextPage">開始</button>-->
-                <!--              <router-link :to="{ name: 'formjudge', params: { met: 'met'}}" >開始</router-link >-->
-            </div>
+        <div class="row mt-5 mb-5">
+            <button id="send_btn" class="btn btn-success col-7 mx-auto"  @click="new SendData(form.id)">決定</button>
         </div>
     </div>
 </template>
