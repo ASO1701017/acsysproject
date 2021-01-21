723c4279c0dad106a148f1f77fd5c1d74706192f
raige <raigekka0128@yahoo.co.jp>
2021-01-21 18:36:46 +0900

    +Update UpdateInformation.vueのデザインを変更

--- src/components/UpdateInformation.vue
+++ src/components/UpdateInformation.vue
@@ -6,118 +6,125 @@
             </div>
         </div>
         <div class="container">
-            <div class="row">
-                <div class="col-3 lead">
-                    日付
+            <div class="mb-4 col-md-9 mx-auto">
+                <div class="h4">1月21日 ver.1.20.21</div>
+                <div class="h5">
+                    <div class="mt-1">カレンダーでの入力が変更</div>
+                    <div class="mt-1">カロリー入力で明日移行入力出来ないようになりました</div>
+                    <div class="mt-1">ログインでの不必要なalertを削除</div>
+                    <div class="mt-1">全体にローディングアニメーションを追加</div>
+                    <div class="mt-1">新規登録の日付をカレンダー入力へ</div>
+                    <div class="mt-1">新規登録に補足を追加</div>
+                    <div class="mt-1">入力系のエラー表示を変更</div>
+                    <div class="mt-1">全体のデザインを調整</div>
+                    <div class="mt-1">Topに戻るの仕組み変更</div>
+                    <div class="mt-1">摂取と消費入力の分類が復活</div>
+                    <div class="mt-1">統計の移動できる年を制限</div>
+                    <div class="mb-2 mt-2">
+                        <div class="mt-1">以下の機能を公開。</div>
+                        <div class="mt-1">・消費カロリーに1日の消費カロリーを追加</div>
+                        <div class="mt-1">・カレンダーからカロリー入力が可能に</div>
+                        <div class="mt-1">・カレンダーからカロリー削除が可能に</div>
+                        <div class="mt-1">・新規登録に利用目的を追加</div>
+                        <div class="mt-1">・登録情報変更に利用目的を追加</div>
+                        <div class="mt-1">・カロリー貯金が増減に対応</div>
+                        <div class="mt-1">・カロリー貯金のコメントを変更</div>
+                        <div class="mt-1">・パスワード再発行完成</div>
+                        <div class="mt-1">・トレーニング機能のコンテンツ増加</div>
+                        <div class="mt-1">・カロリー入力に索引追加</div>
+                        <div class="mt-2">以下のバグを修正</div>
+                        <div class="mt-1">・登録情報変更のデータがリロードしないと切り替わらないのを解決</div>
+                    </div>
                 </div>
-                <div class="col lead ">
-                    内容
-                </div>
-            </div><!-- /.row -->
+            </div><!-- /.row mb-4 -->
             <hr>
-            <div class="row mb-4">
-                <div class="col-3 lead">
-                    11月27日
-                </div>
-                <div class="col lead">
-                    Topページを少し更新しました。
-                    <div class="mb-2">
-                        以下の機能を公開しました。<br>
-                        ・Twitter投稿<br>
-                        ・パスワードの変更<br>
+            <div class="mb-4 col-md-9 mx-auto">
+                <div class="h4">11月17日 ver.1.8.11</div>
+                <div class="h5">
+                    <div class="mt-1">Topページを少し更新</div>
+                    <div class="mb-2 mt-2">
+                        <div class="mt-1">以下の機能を公開</div>
+                            <div class="mt-1">・Twitter投稿</div>
+                            <div class="mt-1">・パスワードの変更</div>
                     </div>
-                    以下のバグを修正しました。<br>
-                    ・新規登録の誕生日に存在しない日付でも登録できるのを修正しました。。<br>
-                    ・カロリー入力のカレンダー表示を修正しました。
+                    <div class="mt-2">以下のバグを修正</div>
+                    <div class="mt-1">・新規登録の誕生日に存在しない日付でも登録できるのを修正</div>
+                    <div class="mt-1">・カロリー入力のカレンダー表示を修正</div>
                 </div>
             </div><!-- /.row mb-4 -->
-            <div class="row mb-4">
-                <div class="col-3 lead">
-                    11月17日
-                </div>
-                <div class="col lead">
-                    Twitterカードが表示されるようになりました！<br>
-                </div>
+            <hr>
+            <div class="mb-4 col-md-9 mx-auto">
+                <div class="h4">11月17日 ver.1.6.9</div>
+                <div class="h5 mt-1">Twitterカードが表示されるようになりました！</div>
             </div><!-- /.row mb-4 -->
-            <div class="row mb-4">
-                <div class="col-3 lead">
-                    11月4日
-                </div>
-                <div class="col lead">
-                    サーバーのログが取れていない問題を解決しました。<br>
-                </div>
+            <hr>
+            <div class="mb-4 col-md-9 mx-auto">
+                <div class="h4">11月4日 ver.1.6.8</div>
+                <div class="h5 mt-1">サーバーのログが取れていない問題を解決</div>
             </div><!-- /.row mb-4 -->
-            <div class="row mb-4">
-                <div class="col-3 lead">
-                    10月31日
-                </div>
-                <div class="col lead">
-                    Topページの反応速度を向上しました。<br>
-                </div>
+            <hr>
+            <div class="mb-4 col-md-9 mx-auto">
+                <div class="h4">10月31日 ver.1.6.7</div>
+                <div class="h5 mt-1">Topページの反応速度を向上</div>
             </div><!-- /.row mb-4 -->
-            <div class="row mb-4">
-                <div class="col-3 lead">
-                    10月19日
-                </div>
-                <div class="col lead">
-                    Topページと更新情報ページの下部に、Twitterシェアボタンと作成者、連絡先を記載しました。<br>
-                </div>
+            <hr>
+            <div class="mb-4 col-md-9 mx-auto">
+                <div class="h4">10月19日 ver.1.6.6</div>
+                <div class="h5 mt-1">Topページと更新情報ページの下部に、Twitterシェアボタンと作成者、連絡先を記載しました</div>
             </div><!-- /.row mb-4 -->
-            <div class="row mb-4">
-                <div class="col-3 lead">
-                    10月10日
-                </div>
-                <div class="col lead">
-                    タイトルをわかりやすく変更しました。<br>
-                </div>
+            <hr>
+            <div class="mb-4 col-md-9 mx-auto">
+                <div class="h4">10月10日 ver.1.5.6</div>
+                <div class="h5 mt-1">タイトルをわかりやすく変更</div>
             </div><!-- /.row mb-4 -->
-            <div class="row mb-4">
-                <div class="col-3 lead">
-                    9月29日
-                </div>
-                <div class="col lead">
-                    Topページのデザインを変更しました。<br>
-                    更新情報を追加しました。<br>
-                    <span class="text-danger mt-3">使えなくなる機能があります！！</span><br>
-                    現在カロリーでの選択入力を使えなくしています。もっと使いやすくなるように改善しますのでもうしばらくお待ちください。
-                </div>
-            </div><!-- /.row mb-4 -->
-            <div class="row mb-4">
-                <div class="col-3 lead">
-                    9月17日
-                </div>
-                <div class="col lead">
-                    Twiiterアカウントを開設しました。新しく出来た機能や更新情報を呟いていきたいと思います。
-                    ぜひフォローしてください！
-                    <a href="https://twitter.com/acsys_calorie">Twitterアカウントはこちら</a>
-                </div>
-            </div><!-- /.row mb-4 -->
-            <div class="row mb-4">
-                <div class="col-3 lead">
-                    8月20日
-                </div>
-                <div class="col lead">
-                    <div class="mb-2">
-                        以下の機能を公開しました。<br>
-                        ・カレンダー<br>
-                        ・統計<br>
-                        ・トレーニング<br>
-                        ・登録情報の変更<br>
+            <hr>
+            <div class="mb-4 col-md-9 mx-auto">
+                <div class="h4">9月29日ver.1.5.3</div>
+                <div class="h5">
+                     <div class="mt-2 mb-2 mt-auto">
+                         <div class="mt-1">Topページのデザインを変更</div>
+                        <div class="mt-1">更新情報を追加</div>
+                         <div>
+                             <div class="text-danger mt-2">使えなくなる機能があります！！</div>
+                            <div class="mt-1">現在カロリーでの選択入力を使えなくしています。もっと使いやすくなるように改善しますのでもうしばらくお待ちください。</div>
+                             </div>
                     </div>
-                    以下のバグを修正しました。<br>
-                    ・新しく摂取カロリーを登録すると、前に登録した摂取カロリーが消えること。<br>
-                    ・統計機能でリストにある貯金の表示ミスを修正しました。
                 </div>
             </div><!-- /.row mb-4 -->
-            <div class="row mb-4">
-                <div class="col-3 lead">
-                    8月14日
-                </div>
-                <div class="col lead">
-                    サイト公開しました。
-                </div>
+            <hr>
+            <div class="mb-4 col-md-9 mx-auto">
+                <span class="h4">9月17日Twiiterアカウント開設</span><br>
+                <span class="h5">
+                     <div class="mt-2 mb-2">
+                        新しく出来た機能や更新情報を呟いていきたいと思います。
+                    ぜひフォローしてください！<a href="https://twitter.com/acsys_calorie">Twitterアカウントはこちら</a>
+                    </div>
+                </span>
+            </div><!-- /.row mb-4 -->
+            <hr>
+            <div class="mb-4 col-md-9 mx-auto">
+                <span class="h4">8月20日 ver.1.4.2</span><br>
+                <span class="h5">
+                     <div class="mt-2 mb-2">
+                        <div class="mt-1">以下の機能を公開</div>
+                        <div class="mt-1">・カレンダー</div>
+                        <div class="mt-1">・統計</div>
+                        <div class="mt-1">・トレーニング</div>
+                        <div class="mt-1">・登録情報の変更</div>
+                    </div>
+                    <div class="mt-1">以下のバグを修正</div>
+                    <div class="mt-1">・新しく摂取カロリーを登録すると、前に登録した摂取カロリーが消えること修正</div>
+                    <div class="mt-1">・統計機能でリストにある貯金の表示ミスを修正</div>
+                </span>
+            </div><!-- /.row mb-4 -->
+            <hr>
+            <div class="mb-4 col-md-9 mx-auto">
+                <div class="h4">8月14日 ver.1.0.0</div>
+                <div class="h5" >本サイト公開しました</div>
             </div><!-- /.row mb-4 -->
         </div><!-- /.container -->
+
+
         <hr class="featurette-divider  mt-5">
         <footer class="container">
             <div class="text-center">
